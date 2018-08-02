0583e81f485eaf09f2c6c8a6524a5709f82bfe7c
Daya6710 <42008636+Daya6710@users.noreply.github.com>
2018-08-02 09:03:51 -0500

    added html

---
+++ javascript.js
@@ -0,0 +1,34 @@
+function shadowhunters(){
+  var video = "<iframe width='560' height='315' src='https://www.youtube.com/embed/hPcD-9JfmgE' frameborder='0' allow='autoplay; encrypted-media' allowfullscreen></iframe>";
+  document.getElementById("demo").innerHTML = "About a young female figuring out a world that was hidden from her." + video;
+}
+
+function grown_ish(){
+  var video = "<iframe width='560' height='315' src='https://www.youtube.com/embed/9bdXZzQvb0U' frameborder='0' allow='autoplay; encrypted-media' allowfullscreen></iframe>";
+  document.getElementById("demo").innerHTML = "Spin off of Black-ish. Its about growing up and going to college." + video;
+}
+
+function cloak_dagger(){
+  var video = "<iframe width='560' height='315' src='https://www.youtube.com/embed/MYr37PT_rI4' frameborder='0' allow='autoplay; encrypted-media' allowfullscreen></iframe>";
+  document.getElementById("demo").innerHTML = "The origin tale of Cloak & Dagger." + video;
+}
+
+function fairy_tail(){
+  var video = "<iframe width='560' height='315' src='https://www.youtube.com/embed/MYr37PT_rI4' frameborder='0' allow='autoplay; encrypted-media' allowfullscreen></iframe>";
+  document.getElementById("demo").innerHTML = "A epic tale about a guild and them growing their powers." + video;
+}
+
+function fairy_tail(){
+  var video = "<iframe width='560' height='315' src='https://www.youtube.com/embed/MYr37PT_rI4' frameborder='0' allow='autoplay; encrypted-media' allowfullscreen></iframe>";
+  document.getElementById("demo").innerHTML = "A epic tale about a guild and them growing their powers." + video;
+}
+
+function fairy_tail(){
+  var video = "<iframe width='560' height='315' src='https://www.youtube.com/embed/MYr37PT_rI4' frameborder='0' allow='autoplay; encrypted-media' allowfullscreen></iframe>";
+  document.getElementById("demo").innerHTML = "A epic tale about a guild and them growing their powers." + video;
+}
+
+function fairy_tail(){
+  var video = "<iframe width='560' height='315' src='https://www.youtube.com/embed/MYr37PT_rI4' frameborder='0' allow='autoplay; encrypted-media' allowfullscreen></iframe>";
+  document.getElementById("demo").innerHTML = "A epic tale about a guild and them growing their powers." + video;
+}
